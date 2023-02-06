1c8845d11f7a81fe7a595288745630ab54aa8f30
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2023-02-06 15:57:54 +0000

    beginning test using TDD pattern

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Test case for module: base_model
+from package: models
+"""
+import unittest
+from models.base_model import BaseModel
+
+class BaseModelTestCase(unittest.TestCase):
+    """TestCase for BaseModel class"""
+    pass
