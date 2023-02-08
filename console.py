ae08e852790b8a6ab2aa73032b7be9f8bb3d2e1d
Ifeanyichukwu Ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2023-02-08 12:34:54 +0000

    implementing Task 7

--- console.py
+++ console.py
@@ -5,6 +5,12 @@ Console contains the entry point of the command interpreter
 """
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
+
+classes = {
+        "BaseModel": BaseModel
+        }
 
 
 class HBNBCommand(cmd.Cmd):
@@ -15,9 +21,85 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
 
+    def do_create(self, line):
+        """Creates a new instance of a given BaseModel
+
+        Args:
+            line (str): the line input to create
+        """
+        command, args, line = self.parseline(line)
+        if command == None:
+            print("** class name missing **")
+        elif command not in classes.keys():
+            print("** class doesn't exist **")
+        else:
+            obj = classes[command]()
+            obj.save()
+            print(obj.id)
+
+    def do_show(self, line):
+        """Prints the string representation of an instance
+        based on the class name and id
+
+        Args:
+            line (str): the line input to show
+        """
+        command, args, line = self.parseline(line)
+        if command == None or command == "":
+            print("** class name missing **")
+        elif command not in classes.keys():
+            print("** class doesn't exist **")
+        elif args == None or args == "":
+            print("** instance id missing **")
+        else:
+            instances = storage.all()
+            instance = None
+            for key,val in instances.items():
+                if val.id == args:
+                    instance = instances[key]
+            if instance:
+                print(instance)
+            else:
+                print("** no instance found **")
+                print("args => ", args)
+
+    def do_destroy(self, line):
+        """Deletes an instance base on the class name and id
+
+        Args:
+            line (str): the input to destroy
+        """
+        command, args, line = self.parseline(line)
+        if command == None or command == "":
+            print("** class name missing **")
+        elif command not in classes.keys():
+            print("** class doesn't exist **")
+        elif args == None or args == "":
+            print("** instance id missing **")
+        else:
+            instances = storage.all()
+            intance_key = None
+            for key, val in instances.items():
+                if val.id == args:
+                    instance_key = key
+            if instance_key:
+                del instances[instance_key]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+
     def do_quit(self, line):
-        """Quit command to exit the program\n"""
+        """Quit command to exit the program
+        
+        Args:
+            line (str): None
+        """
         return True
+
+    def help_quit(self):
+        """Help text to print for quit command"""
+        print("Quit command to exit the program\n")
 
     def do_EOF(self, line):
         """EOF command to exit the program\n"""
