4f6b1326c7eec7849e1f25f263419212ea1e04e9
Ifeanyichukwu Ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2023-02-09 09:44:18 +0000

    Update
    Updates console to reference other class objects
    
    updates instance search to search for instance in the correct class

--- console.py
+++ console.py
@@ -7,11 +7,22 @@ Console contains the entry point of the command interpreter
 import cmd
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from models import storage
+
 
 classes = {
         "BaseModel": BaseModel,
-        "User": User
+        "User": User,
+        "State": State,
+        "Place": Place,
+        "City": City,
+        "Amenity": Amenity,
+        "Review": Review
         }
 
 
@@ -59,7 +70,7 @@ class HBNBCommand(cmd.Cmd):
             for key, val in instances.items():
                 if val.id == args:
                     instance = instances[key]
-            if instance:
+            if instance and instance.to_dict()["__class__"] == command:
                 print(instance)
             else:
                 print("** no instance found **")
