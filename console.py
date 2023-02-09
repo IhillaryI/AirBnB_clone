5753671d5fa7068b14d312f75b5dd90f2b906828
Ifeanyichukwu Ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2023-02-09 08:19:16 +0000

    Update
    Update the command interpreter to allow show, create, destroy, update
    and all used with User.

--- console.py
+++ console.py
@@ -6,10 +6,12 @@ Console contains the entry point of the command interpreter
 
 import cmd
 from models.base_model import BaseModel
+from models.user import User
 from models import storage
 
 classes = {
-        "BaseModel": BaseModel
+        "BaseModel": BaseModel,
+        "User": User
         }
 
 
