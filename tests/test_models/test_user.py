2e0876725d3ecf0cd7b531d2db729a3d4b5fc23a
Ifeanyichukwu Ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2023-02-11 14:24:16 +0000

    update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -5,4 +5,16 @@ Module contains unittest for
 User class
 """
 
+import unittest
 from models.user import User
+
+
+class UserTestCase(unittest.TestCase):
+    """User class Unit tests"""
+    def test_public_attributes(self):
+        """tests the public attributes of User"""
+        user = User()
+        self.assertEqual(user.first_name, "")
+        self.assertEqual(user.email, "")
+        self.assertEqual(user.password, "")
+        self.assertEqual(user.last_name, "")
