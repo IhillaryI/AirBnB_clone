6154d17b5b54925847e26c3284fc70fe94434290
Ifeanyichukwu Ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2023-02-11 14:38:00 +0000

    fixed pycodestyle

--- tests/test_console.py
+++ tests/test_console.py
@@ -10,11 +10,12 @@ from io import StringIO
 from unittest.mock import patch
 from console import HBNBCommand
 
+
 class ConsoleTestCase(unittest.TestCase):
 
     def test_help(self):
         """tests the help command"""
-        with patch('sys.stdout', new = StringIO()) as f:
+        with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("help")
             HBNBCommand().onecmd("help create")
             HBNBCommand().onecmd("help show")
@@ -22,7 +23,7 @@ class ConsoleTestCase(unittest.TestCase):
 
     def test_all(self):
         """tests the show command"""
-        with patch('sys.stdout', new = StringIO()) as f:
+        with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("all")
             HBNBCommand().onecmd("all User")
             HBNBCommand().onecmd("User.all()")
